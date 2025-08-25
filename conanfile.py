09817690070dfde620e5fcfb84c7feb2ade00595
DefinitelyBak <Halyl555@gmail.com>
2025-08-25 14:08:19 +0300

    Правки для CI.

--- conanfile.py
+++ conanfile.py
@@ -1,9 +1,19 @@
 from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import CMake
 
 class CompressorRecipe(ConanFile):
     settings = "os", "compiler", "build_type", "arch"
-    generators = "CMakeToolchain", "CMakeDeps"
+    generators = "CMakeDeps", "CMakeToolchain"
 
     def requirements(self):
         self.requires("gtest/1.16.0")
         self.requires("poco/1.13.3")
+
+    def layout(self):
+        cmake_layout(self)
+
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
