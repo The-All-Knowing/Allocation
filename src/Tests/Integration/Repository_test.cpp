14441c5c7649e60829302c3b6dd9c3a0cc6bbbf7
DefinitelyBak <Halyl555@gmail.com>
2025-09-11 16:37:28 +0300

    Проба.

--- src/Tests/Integration/Repository_test.cpp
+++ src/Tests/Integration/Repository_test.cpp
@@ -12,22 +12,15 @@ namespace Allocation::Tests
 {
     TEST_F(Database_Fixture, test_get_by_batchref)
     {
-        try
-        {
-            Adapters::Repository::SqlRepository repo(_session);
-            Domain::Batch b1("b1", "sku1", 100);
-            Domain::Batch b2("b2", "sku1", 100);
-            Domain::Batch b3("b3", "sku2", 100);
-            auto p1 = std::make_shared<Domain::Product>("sku1", std::vector<Domain::Batch>{b1, b2});
-            auto p2 = std::make_shared<Domain::Product>("sku2", std::vector<Domain::Batch>{b3});
-            repo.Add(p1);
-            repo.Add(p2);
-            EXPECT_EQ(repo.GetByBatchRef("b2"), p1);
-            EXPECT_EQ(repo.GetByBatchRef("b3"), p2);
-        }
-        catch (const Poco::Exception& e)
-        {
-            std::cerr << e.displayText(); 
-        }
+        Adapters::Repository::SqlRepository repo(_session);
+        Domain::Batch b1("b1", "sku1", 100);
+        Domain::Batch b2("b2", "sku1", 100);
+        Domain::Batch b3("b3", "sku2", 100);
+        auto p1 = std::make_shared<Domain::Product>("sku1", std::vector<Domain::Batch>{b1, b2});
+        auto p2 = std::make_shared<Domain::Product>("sku2", std::vector<Domain::Batch>{b3});
+        repo.Add(p1);
+        repo.Add(p2);
+        EXPECT_EQ(repo.GetByBatchRef("b2"), p1);
+        EXPECT_EQ(repo.GetByBatchRef("b3"), p2);
     }
 }
